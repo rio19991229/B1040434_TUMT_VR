e71e58c097602f93864c13c5a6dabad4e26c5a13
rio19991229 <55218512+rio19991229@users.noreply.github.com>
2019-12-19 10:45:53 +0800

    12/19

---
+++ B1040434_TUMT_VR/Assets/script/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static bool havekey;
+
+
+    //公開並且參數零或一個
+
+    public void ThrowPickup()
+    {
+        print("撿起物件");
+    }
+    public void ThrowDetech(GameObject obj)
+    {
+        print("放開物件");
+        havekey = true;
+        Destroy(obj);
+
+    }
+    public void ThrowHeld()
+    {
+        print("拿著物件");
+    }
+
+
+
+
+
+
+
+
+}
+
+
---
+++ B1040434_TUMT_VR/Assets/script/TeleportScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class TeleportScript : MonoBehaviour
+{
+    [Header("動畫控制器")]
+    public Animator aniContorl;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "HeadCollider"&&GameManager.havekey)
+        {
+            aniContorl.SetBool("character_nearby", true);
+        }
+    }
+
+}
