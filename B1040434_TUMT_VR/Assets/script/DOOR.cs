9ce55470772d177823cfdfd1d4331694d6fa4828
rio19991229 <55218512+rio19991229@users.noreply.github.com>
2019-12-26 10:09:32 +0800

    1226

---
+++ B1040434_TUMT_VR/Assets/script/DOOR.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class DOOR : MonoBehaviour
+{
+    private Animator ani;
+
+    private void Start()
+    {
+        ani = GetComponent<Animator>();
+    
+    }
+    public void OpenDoor()
+    {
+        ani.SetBool("character_nearby",true);
+    }
+    
+
+
+
+
+}
